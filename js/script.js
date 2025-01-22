63ab5655dc197e0647c5d240434f35573b5339f2
Segun David <segdavid03@gmail.com>
2025-01-22 15:03:56 +0100

    refactor: removed redundant code

--- js/script.js
+++ js/script.js
@@ -2,26 +2,38 @@
 
 const activeBar = document.querySelectorAll(".tab");
 
-// iterate through each nodes
-activeBar.forEach((tab) => {
+// // iterate through each nodes
+// activeBar.forEach((tab) => {
+//   tab.addEventListener("click", () => {
+//     if (tab === activeBar[0]) {
+//       // remove active from tabs
+//       activeBar[1].classList.remove("active");
+//       activeBar[2].classList.remove("active");
+//       // add active class to clicked tab
+//       activeBar[0].classList.add("active");
+//     } else if (tab === activeBar[1]) {
+//       // remove classes from other tabs
+//       activeBar[0].classList.remove("active");
+//       activeBar[2].classList.remove("active");
+//       // add active class to clicked tab
+//       activeBar[1].classList.add("active");
+//     } else if (tab === activeBar[2]) {
+//       activeBar[0].classList.remove("active");
+//       activeBar[1].classList.remove("active");
+//       // add active class to clicked tab
+//       activeBar[2].classList.add("active");
+//     }
+//   });
+// });
+
+// refactor code to be more efficient and readable
+activeBar.forEach((tab, index) => {
   tab.addEventListener("click", () => {
-    if (tab === activeBar[0]) {
-      // remove active from tabs
-      activeBar[1].classList.remove("active");
-      activeBar[2].classList.remove("active");
-      // add active class to clicked tab
-      activeBar[0].classList.add("active");
-    } else if (tab === activeBar[1]) {
-      // remove classes from other tabs
-      activeBar[0].classList.remove("active");
-      activeBar[2].classList.remove("active");
-      // add active class to clicked tab
-      activeBar[1].classList.add("active");
-    } else if (tab === activeBar[2]) {
-      activeBar[0].classList.remove("active");
-      activeBar[1].classList.remove("active");
-      // add active class to clicked tab
-      activeBar[2].classList.add("active");
-    }
+    // Remove the "active" class from all tabs
+    activeBar.forEach((t) => {
+      t.classList.remove("active");
+    });
+    // Add the "active" class to the clicked tab
+    tab.classList.add("active");
   });
 });
