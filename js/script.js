6179be6b99b0a4f38c32fb23f898f1bb862e98b6
Segun David <segdavid03@gmail.com>
2025-01-22 17:02:27 +0100

    style: styled pricing section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const activeBar = document.querySelectorAll(".tab");
+const featuresTab = document.querySelectorAll(".features__tab1");
 
 // // iterate through each nodes
 // activeBar.forEach((tab) => {
@@ -29,6 +30,18 @@ const activeBar = document.querySelectorAll(".tab");
 // refactor code to be more efficient and readable
 activeBar.forEach((tab, index) => {
   tab.addEventListener("click", () => {
+    // add "hidden" class to all features Tab
+    featuresTab.forEach((ele) => {
+      ele.classList.add("hidden");
+      // Add show class to the corresponding tab
+      if (tab === activeBar[1]) {
+        featuresTab[1].classList.remove("hidden");
+      } else if (tab === activeBar[2]) {
+        featuresTab[2].classList.remove("hidden");
+      } else {
+        featuresTab[0].classList.remove("hidden");
+      }
+    });
     // Remove the "active" class from all tabs
     activeBar.forEach((t) => {
       t.classList.remove("active");
@@ -37,3 +50,5 @@ activeBar.forEach((tab, index) => {
     tab.classList.add("active");
   });
 });
+
+console.log(featuresTab);
